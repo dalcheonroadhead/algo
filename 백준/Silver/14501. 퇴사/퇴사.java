b0c0c26d7b4ba22c3427f5d024d50315d6c2bf10
Dalcheon_Dong_Teeth_Breaker <102154788+dalcheonroadhead@users.noreply.github.com>
2023-08-20 21:45:39 +0900

    [Silver III] Title: 퇴사, Time: 136 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -8,78 +8,45 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int sum, N;
-    static ArrayList<int[]> list = new ArrayList<>();
-    static boolean [] flag;
-    static int max = 0;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        list.add(0, null);
-        flag = new boolean[N+1];
+        int  N = Integer.parseInt(br.readLine());
+
+        // 문제에서는 1일 부터 시작하므로, 배열도 0이 아닌 1부터쓰기 위해 N+1 한 것
+        // dp의 경우 N+1칸도 N일에 대한 값을 구하기 위해 사용됨으로, N+2부터 시작.
+        int [] T = new int[N+1];
+        int [] P = new int[N+1];
+        int [] dp = new int[N+2];
+
 
         for (int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-
-            int remainedDay = N-i;
-            int Time = Integer.parseInt(st.nextToken());
-            int Pay = Integer.parseInt(st.nextToken());
-
-            if(remainedDay - Time+1 < 0) {
-                int [] pair = {-1, 0};
-                list.add(pair);
-                continue;
-            };
-
-            int [] pair = {Time,Pay};
-            list.add(pair);
-
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 1; i < list.size(); i++) {
-            if(list.get(i)[0] == -1){
-                flag[i] = true;
+        // 역순으로 뒤에서 부터 쌓아서 온다.
+        for (int i = N; i > 0 ; i--) {
+
+            // 10일날 퇴사(N = 10)인데, 10일째에 당일 하루만 일해도 되는 경우 i + T[i]는 11일로 이것도 포함시켜줘야 함.
+            // 따라서 N+1로 하는 것
+            if( i + T[i] > N+1){
+                // default 초기화 이용
+                dp[i] = dp[i+1];
             }
-        }
 
-        calcul(1);
-        System.out.println(max);
-    }
-
-    // true - 방문, false - 미방문
-    public static void calcul(int index){
-
-        if(index >= list.size()){
-            max = Math.max(max, sum);
-            return;
-        }
-
-        if(list.get(index)[0] == -1){
-            calcul(index+1);
-            return;
-        }
-
-
-        for (int i = index; i < list.size(); i++) {
-
-            if(flag[i]) continue;
-
-            for (int j = i; j < list.get(i)[0]+i; j++) {
-                if(list.get(j)[0] == -1) continue;
-                flag[j] = true;
+            else{
+                // 당일 일 안하고, 그 후 일해서 돈 벌었을 때랑, 당일 일하고, 일이 끝난 다음 일 했을 때 중 큰 값을 뽑는다.
+                dp[i] = Math.max(dp[i+1], P[i] + dp[i + T[i]] );
             }
-            sum += list.get(i)[1];
-            calcul(list.get(i)[0]+i);
 
-            for (int j = i; j < list.get(i)[0]+i; j++) {
-                if(list.get(j)[0] == -1) continue;
-                flag[j] = false;
-            }
-            sum -=list.get(i)[1];
-            calcul(i+1);
-            return;
         }
+
+        // dp1은 최종적으로 첫 날까지 모두 계산하여, 최적의 값이 나온다. 
+        System.out.println(dp[1]);
+
     }
 }
