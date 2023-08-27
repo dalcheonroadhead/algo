1d496f0d21ba5034e8426355e7391cf60daa5f62
Dalcheon_Dong_Teeth_Breaker <102154788+dalcheonroadhead@users.noreply.github.com>
2023-08-27 19:06:52 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1452 ms, Memory: 130092 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        StringBuilder sb = new StringBuilder();
+
+       ArrayList<Integer> list =new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+
+        for(int a: list){
+            sb.append(a).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+
+}
