1cfff9a96b38022d4f53834b40ffdd6de8e1d67c
Dalcheon_Dong_Teeth_Breaker <102154788+dalcheonroadhead@users.noreply.github.com>
2023-08-27 19:21:14 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 708 ms, Memory: 55560 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        StringBuilder sb = new StringBuilder();
+
+        ArrayList<Coordinate> list =new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            list.add(new Coordinate(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+
+        }
+
+        Collections.sort(list);
+
+        for (int j = 0; j < list.size(); j++) {
+            sb.append(list.get(j).x).append(" ").append(list.get(j).y).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
+
+class Coordinate implements Comparable<Coordinate>{
+    int x;
+    int y;
+
+    public Coordinate(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+
+    @Override
+    public int compareTo(Coordinate o) {
+        if(this.x == o.x){
+            return this.y - o.y;
+        }
+        return this.x - o.x;
+    }
+}
