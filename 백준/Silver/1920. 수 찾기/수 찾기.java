8a6638a1b096c35e2e73ad02b4d94360b43064da
Dalcheon_Dong_Teeth_Breaker <102154788+dalcheonroadhead@users.noreply.github.com>
2023-09-19 22:18:52 +0900

    [Silver IV] Title: 수 찾기, Time: 708 ms, Memory: 45924 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int [] arr= new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        int M = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        loopout:
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            int start = 0;
+            int end = N-1;
+            int mid = 0;
+            while (start <= end){
+                mid = (start+end)/2;
+
+                if(arr[mid] == target){
+                    sb.append(1).append("\n");
+                    continue loopout;
+                }
+                else if (arr[mid] > target){
+                    end = mid-1;
+                } else if (arr[mid] < target) {
+                    start = mid+1;
+                }
+            }
+            sb.append(0).append("\n");
+
+        }
+
+        System.out.println(sb);
+
+    }
+}
