36035ac2723cc29dbb47ecf62883605d29274d92
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-04-08 13:59:32 +0900

    [Silver III] Title: N과 M (1), Time: 276 ms, Memory: 22456 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,56 +1,51 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    // 전역에서 배열을 쓰기 위해 선언과 할당을 따로
-    static boolean[] visted;
-    static int[] arr;
+    // N은 내가 가용할 수 있는 수
+    // M은 내가 나열할 숫자의 수
+    static int N,M;
+    static boolean [] isVisted;
+    static int [] arr;
+
+    static StringBuilder sb = new StringBuilder();
+
 
     public static void main(String[] args) throws IOException {
+
+        // 값 입력 받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 자연수 - N, 선택할 총량 - M
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-         visted = new boolean[N];
-         arr = new int[M];
+        isVisted = new boolean[N];
+        arr = new int[M];
+        cal(0);
 
-        calc(N,M,0);
-
+        System.out.println(sb);
     }
 
-    /*
-    * 백트래킹을 재귀를 이용해 구현 -> 방문: true, 방문x -> false
-    */
+    public static void cal(int deepth) {
 
-    public static void calc(int N, int M, int deepth){
         if(deepth == M){
-            for (int a :
-                    arr) {
-                System.out.print(a+ " ");
+            for (int i = 0; i < arr.length; i++) {
+                sb.append(arr[i]).append(" ");
             }
-            System.out.println();
+            sb.append("\n");
             return;
         }
 
+
         for (int i = 0; i < N; i++) {
-
-            // 방문했다면, 방문한 번호를 저장 및 방문 했다고 팻말 바꾸기
-            if(!visted[i]){
-                visted[i] = true;
-                arr[deepth] = i+1; // 실제 값은 index +1
-                calc(N,M,deepth+1);
-                visted[i] = false;
+            if(!isVisted[i]){
+                arr[deepth] = i+1;
+                isVisted[i] = true;
+                cal(deepth+1);
+                isVisted[i] = false;
             }
-            // 만약 arr 다 채우고 프린트 해서 나왔다면,
-
         }
     }
 }
