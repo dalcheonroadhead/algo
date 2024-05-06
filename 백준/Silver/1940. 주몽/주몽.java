f9f9d2f4adeddb739a5cf96b23d6b80ae8d851f1
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-05-06 16:58:13 +0900

    [Silver IV] Title: 주몽, Time: 192 ms, Memory: 16612 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 
@@ -11,44 +12,35 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M;
-    static int [] arr;
-    static int [] target = new int [2];
-    static int cnt = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 1. 값 받기
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
-        arr = new int[N];
-
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int [] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        combination(0,0);
-        System.out.println(cnt);
-    }
+        Arrays.sort(arr);
 
-    public static void combination (int depth, int start) {
-        if(depth == 2){
+        int start = 0, end = N-1, cnt = 0;
 
-            int ans = 0;
-            for (int i = 0; i < target.length; i++) {
-                ans +=target[i];
-            }
-            if(ans == M) {
+        while (start != end) {
+            int value = arr[start] + arr[end];
+
+            if(value > M){
+                end--;
+            } else if (value == M) {
                 cnt ++;
+                end --;
+            } else {
+                start ++;
             }
-            return;
         }
 
-        for (int i = start; i < N; i++) {
-           target[depth] = arr[i];
-           combination(depth+1, i+1);
-        }
+        System.out.println(cnt);
+
     }
 }
