b346485ed5d14670c8058eec231a740fad3950f9
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-06-16 23:42:37 +0900

    [level 1] Title: 폰켓몬, Time: 1.66 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i ++){
+            map.putIfAbsent(nums[i], 1);
+        }
+        
+        if(map.size() < nums.length/2) {
+            return map.size();
+        }else{
+            return nums.length/2;
+        }
+        
+        
+
+        
+    }
+}
