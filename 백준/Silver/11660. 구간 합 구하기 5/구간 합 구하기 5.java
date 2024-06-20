190fb89abdc4a8fc29b12f4885a3115766acea6c
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-06-21 00:50:50 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 952 ms, Memory: 122628 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -9,33 +9,35 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int [][] s = new int [N+1][N+1];
+        int [][] sum = new int [N+1][N+1];
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j <= N; j++) {
-                s[i][j] = j==0? s[i-1][N] : s[i][j-1] + Integer.parseInt(st.nextToken());
+            for (int j = 1; j <= N; j++) {
+                // 1) 좌, 상 테두리 부분 계산 -> 1차원 배열 누적 합
+                if(i == 1) {
+                    sum[i][j] = sum[i][j-1] + Integer.parseInt(st.nextToken());
+                }
+                else if(j == 1) {
+                   sum[i][j] = sum[i-1][j] + Integer.parseInt(st.nextToken());
+                }
+                // 2) 좌상단부터 현 위치 까지의 사각형 누적합 계산
+                else {
+                    sum[i][j] = sum[i][j-1] + sum[i-1][j]  - sum[i-1][j-1] + Integer.parseInt(st.nextToken());
+                }
             }
         }
 
         StringBuilder sb = new StringBuilder();
-
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int sX = Integer.parseInt(st.nextToken());
-            int sY = Integer.parseInt(st.nextToken());
-            int eX = Integer.parseInt(st.nextToken());
-            int eY = Integer.parseInt(st.nextToken());
+            int sx = Integer.parseInt(st.nextToken());
+            int sy = Integer.parseInt(st.nextToken());
+            int ex = Integer.parseInt(st.nextToken());
+            int ey = Integer.parseInt(st.nextToken());
 
-            int sum = 0;
-
-            for (int j = sX; j <= eX; j++) {
-                sum += (s[j][eY] - s[j][sY-1]);
-            }
-
-            sb.append(sum).append("\n");
+            sb.append((sum[ex][ey] - sum[ex][sy-1] - sum[sx-1][ey] + sum[sx-1][sy-1])).append("\n");
         }
-
         System.out.println(sb);
     }
 }
