b69947f8908201f99f7d7aecb95e6ffa6e39879b
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-06-20 17:51:33 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 488 ms, Memory: 52580 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,63 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static ArrayList<Integer> list = new ArrayList<>();
-	static ArrayList<Integer> accSums = new ArrayList<>();
-	static int N,M,sum = 0;
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		  // 자연수 N까지 
-		  N = Integer.parseInt(st.nextToken());
-		
-		  // i와 j 쌍의 갯수
-		  M = Integer.parseInt(st.nextToken());
-		 
-		  st = new StringTokenizer(br.readLine());
-		  for(int i = 0; i < N; i++) {
-			  list.add(Integer.parseInt(st.nextToken()));
-		  }
-		  
-		  acc(0);
-		  
-		  for(int i = 0; i< M; i++) {
-			  st = new StringTokenizer(br.readLine());  
-			  int base = Integer.parseInt(st.nextToken())-1;
-			  int deepth = Integer.parseInt(st.nextToken())-1;
-			  
-			  if(base == deepth) {
-				 System.out.println(list.get(base));
-			  }else if(base >0) {
-				  System.out.println(accSums.get(deepth) - accSums.get(base-1));
-			  }else if(base <=0) {
-				  System.out.println(accSums.get(deepth));
-			  }
-		  }
-		  
-		  
-		  
-	}
-	
-	public static void acc(int deepth) {
-		if(deepth == N) {
-			return;
-		}
-		
-		
-		sum +=list.get(deepth);
-		accSums.add(deepth, sum);
-		acc(deepth+1);
-		
-	}
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int [] sum = new int [N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            // 누적합
+            if(i == 0){
+                sum[i] = Integer.parseInt(st.nextToken());
+            }else{
+                sum[i] = sum[i-1] + Integer.parseInt(st.nextToken());
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken())-2;
+            int b = Integer.parseInt(st.nextToken())-1;
+
+            sb.append(a < 0? sum[b] : (sum[b]- sum[a])).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
