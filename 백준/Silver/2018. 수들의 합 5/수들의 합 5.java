3d19692960fd7c6a492de85b6fab0dc3342d6056
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-06-28 14:36:21 +0900

    [Silver V] Title: 수들의 합 5, Time: 164 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -2,46 +2,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
-/*
- * 2018번 수들의 합 5
- * */
-
-
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
+        int cnt = 0, start = 0, end = 0, acc = 0;
 
+        // end++로 갱신된 값은 다음 루프부터 영향을 주므로, N+1을 해야 15까지 온전하게 더할 수 있다.
+         while (end <= N){
 
-        // a    -> 왼 pointer, b -> 오 포인터,
-        // acc  -> 축적값 (맨 처음에 더해지는 1은 더했다.)
-        // cnt  -> N = N 즉 자기 자신으로 구간합이 되는 경우는 미리 세고 간다.
-        int a = 0, b = 0, acc = 0, cnt = 1;
-
-        // 오른쪽 포인터가 N이 되는 순간 구간합은 무조건 N보다 커진다. 그래서 셀 필요가 없다.
-        while (b != N) {
-
-            if(acc < N){
-                b++;
-                acc +=b;
-            }
-
-            else if(acc == N) {
-                cnt ++;
-                b ++;
-                acc += b;
-            }
-
-            else {
-                acc -= a;
-                a++;
-            }
-        }
+             if(acc < N){
+                 end++;
+                 acc += end;
+             }else {
+                 if (acc == N){
+                     cnt++;
+                     end++;
+                     acc += end;
+                 }else {
+                     start++;
+                     acc -= start;
+                 }
+             }
+         }
 
         System.out.println(cnt);
-
     }
 }
