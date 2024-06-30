73e81d5d054037d3c02140f78f7e0e6f63d7e002
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-06-30 16:27:00 +0900

    [Silver II] Title: 스택 수열, Time: 288 ms, Memory: 26596 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,63 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
-
-/*
-* 1874번 스택 수열
-* */
-
+import java.io.*;
 
 public class Main {
 
-    static int [] ans;
-
     public static void main(String[] args) throws IOException {
-
-        // 1. 값 입력 ---------------------------------------------------------
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        ans = new int [N];
+        int [] stack = new int [N];
+        int [] ans = new int [N];
+        int stackIter = 0;
+        int ansIter = 0;
 
         for (int i = 0; i < N; i++) {
             ans[i] = Integer.parseInt(br.readLine());
         }
 
-        // [stack]을 배열로 구현
-        int [] stack = new int [N];
+        for (int i = 0; i < N; i++) {
+            // 답 배열의 현재 바라보고 있는 원소보다 i값이 작거나 같다면 stack에 삽입
+            // stack에 값이 답 배열의 현 원소와 같다면 꺼내기
+            // 답 배열의 현 원소보다 스택에 값이 크다면 그것은 만들 수 없는 수열이므로 NO 출력 후 종료
 
-        // top은 Stack의 지시자,  stack 배열에 값이 찰수록 올림. -> 뺄 때마다 내림
-        int top = -1;
-        StringBuilder sb = new StringBuilder();
-        int index = 0;
-
-        // stack 에 값 넣으면서 바로바로
-        for (int i = 1; i <= N; i++) {
-
-            // 1) 같은 경우가 아니라면 stack에 오름차순으로 값을 push 한다.
-            stack[++top] = i;
-            sb.append("+\n");
-
-            // 2)  stack에 값이 존재하고, stack의 탑이랑 뽑아야할 값이랑 같으면 POP한다.
-            while (top != -1 && stack[top] == ans[index]){
-
-                top--;                      // 지시자 한 단계 내리기 pop을 구현한 것
-                index++;                    // 지금 답은 이제 해결 했음으로 다음으로 넘어간다.
-                sb.append("-\n");           // 값 넣기
+            if(i == 0 || stackIter <= 0){
+                stack[stackIter++] = i+1;
+                sb.append("+").append("\n");
+            } else if(ans[ansIter] >= stack[stackIter-1]){
+                stack[stackIter++] = i+1;
+                sb.append("+").append("\n");
             }
+
+            while(ans[ansIter] == stack[stackIter-1]){
+                sb.append("-").append("\n");
+                ansIter++;
+
+                stack[stackIter-1] = -1;
+                stackIter--;
+
+                if(ansIter == N || stackIter == 0) break;
+
+            }
+
+            if(stackIter !=0 && ans[ansIter] < stack[stackIter-1]) {
+                System.out.println("NO");
+                return;
+            }
+
         }
 
-        // 3) Stack이 empty가 아니고, stack의 peek이 답의 값과 같으면 pop
-        while (top != -1 && stack[top] == ans[index]){
-            top --;
-            index ++;
-            sb.append("-\n");
-        }
+        System.out.println(sb);
 
-        // 4) 3번에서 [stack]의 peek 과 현재 출력해야 하는 답 부분이 어긋나면 해당 문제는 풀 수 없는 문제 이므로
-        //    stack 배열을 맨 아래까지 보지 않은 채, while loop를 나간다.
-        //    따라서 top이 맨 밑바닥인 -1까지 오지 않으면 NO를 출력한다.
-        System.out.println(top == -1? sb : "NO");
     }
 }
