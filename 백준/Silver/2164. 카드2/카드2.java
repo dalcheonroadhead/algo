06bc84fc2d10c4019a3441a609b46f07b804a5b4
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-07-01 14:49:44 +0900

    [Silver IV] Title: 카드2, Time: 176 ms, Memory: 28968 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,40 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
-
+import java.io.*;
+import java.util.ArrayDeque;
 public class Main {
-	
-	
-    public static void main(String[] args) throws NumberFormatException, IOException {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        Deque<Integer> dq = new ArrayDeque<>();
-        
-        // 1. 카드 수 입력 받기
         int N = Integer.parseInt(br.readLine());
-        
-        // 1-1. 1~N까지 차례대로 카드 덱으로 구성 
-        for(int i = 1; i <= N; i++) {
-        	dq.addLast(i);
+        ArrayDeque<Integer> aq1 = new ArrayDeque<>();
+
+        for (int i = 0; i < N; i++) {
+            aq1.add(i+1);
         }
-        
-        
-        // 2. while loop 돌리기 
-        while(true) {
-        	
-        	//** 탈출 조건 q안이 1개 남았을 경우**
-        	if(dq.size() == 1) break;
-        	
-        	// 3. 맨 앞의 숫자는 버리기
-        	dq.pollFirst();
-            // 4. 그 다음으로 맨 앞 숫자가 되는 녀석은 Deck의 맨 밑으로 보내기 
-            dq.addLast(dq.poll());
-        	// 5. 이것을 반복 	
+
+        while (aq1.size() != 1){
+            // 맨 위에꺼 하나 빼기
+            aq1.poll();
+            // 맨 위에꺼 밑으로 넣기
+            aq1.add(aq1.poll());
         }
-        
-        System.out.println(dq.pollFirst());
-        
-        
-    }   
+
+        System.out.println(aq1.peek());
+
+    }
 }
