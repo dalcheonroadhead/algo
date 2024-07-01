e2ab12a2e31b6293d3e21c2b8b04b7f233b9f05d
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-07-01 15:33:17 +0900

    [Silver I] Title: 절댓값 힙, Time: 280 ms, Memory: 24700 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -10,12 +10,9 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {    // return 결과가 양수면 자리 바꾸고, 음수면 자리 안 바꿈.
-                if(Math.abs(o1) == Math.abs(o2)) return Integer.compare(o1,o2);
-                return Math.abs(o1) - Math.abs(o2);
-            }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {    // return 결과가 양수면 자리 바꾸고, 음수면 자리 안 바꿈.
+            if(Math.abs(o1) == Math.abs(o2)) return Integer.compare(o1,o2);
+            return Math.abs(o1) - Math.abs(o2);
         });
 
         for (int i = 0; i < N; i++) {
