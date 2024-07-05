ae969b78b9f539f0e94ca5c4eb9615c691f48581
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-07-06 00:03:47 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 964 ms, Memory: 103156 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,34 +1,68 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
+
 
 public class Main {
-
+    static int N;
+    static int [] arr;
+    static int [] temp;
 
     public static void main(String[] args) throws IOException {
-
-        StringBuilder sb = new StringBuilder();
-
-       ArrayList<Integer> list =new ArrayList<>();
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        arr = new int [N];
+        temp = new int [N];
 
         for (int i = 0; i < N; i++) {
-            list.add(Integer.parseInt(br.readLine()));
+            arr[i] = Integer.parseInt(br.readLine());
         }
+        merge_sort(0, N-1);
 
-        Collections.sort(list);
-
-        for(int a: list){
-            sb.append(a).append("\n");
-        }
-
+       StringBuilder sb = new StringBuilder();
+       for (int temp : arr){
+           sb.append(temp).append("\n");
+       }
         System.out.println(sb);
-
     }
 
+    public static void merge_sort(int start, int end){
+        // 0. 기저 조건
+        if(end - start == 0) return;
+        // 1. 계산
+        int mid = (start+end)/2;
+        // 분할 정복
+        merge_sort(start, mid);
+        merge_sort(mid+1, end);
+        // 현재까지의 정렬 상태를 temp 값에 담기
+        for (int i = start; i <= end; i++) {
+            temp[i] = arr[i];
+        }
+        // 3가지 포인터를 이용한 병합 정렬
+        int O = start;
+        int i = start;
+        int j = mid+1;
 
+        // 포인터로 넣기
+        while (i <= mid && j <= end){
+            if(temp[i] < temp[j]){
+                arr[O] = temp[i];
+                i++;
+            }else{
+                arr[O] = temp[j];
+                j++;
+            }
+            O++;
+        }
+
+        if(i <= mid){
+            for (int k = i; k <= mid; k++) {
+                arr[O++] = temp[k];
+            }
+        }
+        if(j <= end) {
+            for (int k = j; k <= end; k++) {
+                arr[O++] = temp[k];
+            }
+        }
+    }
 }
