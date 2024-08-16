761de5afee593175775f8862fd4400a910b721ff
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-08-16 17:26:28 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 28.32 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    // 1. map에 값 저장 
+    // 2. 저장한 개인정보를 기준으로, 파기해야할 날짜 계산 
+        // (1) 달 + 저장기간 
+        // (2) 달 + 저장기간이 12개월을 초과하면, 년도 +1
+        // (3) 일은 현재 일자 -1 (만약 1일이라면 28일로 변경)
+    // 3. today랑 비교해서 현재 파기해야하는지 확인
+    
+    static ArrayList<Integer> ans = new ArrayList<>();
+    
+    static HashMap<String, Integer> map = new HashMap<>();
+    static int nowYear  = 0;
+    static int nowMonth = 0;
+    static int nowDays  = 0;
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        
+        StringTokenizer dDay = new StringTokenizer(today, ".", false);
+        nowYear     = Integer.parseInt(dDay.nextToken());
+        nowMonth    = Integer.parseInt(dDay.nextToken());
+        nowDays     = Integer.parseInt(dDay.nextToken());
+        
+        
+        // map에 값 저장
+        for(int i = 0; i < terms.length; i ++){
+            StringTokenizer st = new StringTokenizer(terms[i], " ", false);
+            map.put(st.nextToken(), Integer.parseInt(st.nextToken()));
+        }
+        // 만료일 계산
+        for(int i = 0; i < privacies.length; i++){
+            StringTokenizer outer = new StringTokenizer(privacies[i]);
+            StringTokenizer inner = new StringTokenizer(outer.nextToken(), ".", false);
+            String term = outer.nextToken();
+            int expire = map.get(term);
+            
+            int year    = Integer.parseInt(inner.nextToken());
+            int month   = Integer.parseInt(inner.nextToken());
+            int days    = Integer.parseInt(inner.nextToken());
+            
+            month += expire;
+            
+            if(month > 12){
+                if(month%12 == 0){
+                    year += month/12 -1;
+                    month = 12;
+                }else {
+                    year += month/12;
+                    month = month%12;
+                }
+            }
+            
+            days -= 1; 
+            if(days == 0){
+                month --;
+                days = 28;
+            }
+            
+            System.out.printf("파기 날짜: %d.%d.%d\n", year, month, days);
+            
+            if(nowYear > year) {
+                ans.add((i+1)); 
+            }else if(nowYear == year && nowMonth > month){
+                ans.add((i+1)); 
+            }else if(nowYear == year && nowMonth == month && nowDays > days){
+                ans.add((i+1)); 
+            }
+        }
+        return ans.stream().mapToInt(i -> i).toArray();
+    }
+}
