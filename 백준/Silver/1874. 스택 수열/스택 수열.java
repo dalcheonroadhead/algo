e5b46db2f70c42bfd5667ede2aed878512c3a615
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-08-18 14:51:48 +0900

    [Silver II] Title: 스택 수열, Time: 256 ms, Memory: 25392 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,53 +1,31 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        BufferedReader br   = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb    = new StringBuilder();
+        int N               = Integer.parseInt(br.readLine());
+        int [] stack        = new int[N+1];
+        int top             = 0;
+        int value           = 1;
 
-        int N = Integer.parseInt(br.readLine());
-        int [] stack = new int [N];
-        int [] ans = new int [N];
-        int stackIter = 0;
-        int ansIter = 0;
-
-        for (int i = 0; i < N; i++) {
-            ans[i] = Integer.parseInt(br.readLine());
-        }
-
-        for (int i = 0; i < N; i++) {
-            // 답 배열의 현재 바라보고 있는 원소보다 i값이 작거나 같다면 stack에 삽입
-            // stack에 값이 답 배열의 현 원소와 같다면 꺼내기
-            // 답 배열의 현 원소보다 스택에 값이 크다면 그것은 만들 수 없는 수열이므로 NO 출력 후 종료
-
-            if(i == 0 || stackIter <= 0){
-                stack[stackIter++] = i+1;
-                sb.append("+").append("\n");
-            } else if(ans[ansIter] >= stack[stackIter-1]){
-                stack[stackIter++] = i+1;
-                sb.append("+").append("\n");
+        for (int i = 1; i <= N ; i++) {
+            int now = Integer.parseInt(br.readLine());
+            while (value <= now) {
+                if(value > N) break;
+                stack[++top] = value++;
+                sb.append("+\n");
             }
-
-            while(ans[ansIter] == stack[stackIter-1]){
-                sb.append("-").append("\n");
-                ansIter++;
-
-                stack[stackIter-1] = -1;
-                stackIter--;
-
-                if(ansIter == N || stackIter == 0) break;
-
+            if(stack[top] == now){
+                top--;
+                sb.append("-\n");
             }
-
-            if(stackIter !=0 && ans[ansIter] < stack[stackIter-1]) {
+            else if(stack[top] > now){
                 System.out.println("NO");
                 return;
             }
-
         }
-
         System.out.println(sb);
-
     }
 }
