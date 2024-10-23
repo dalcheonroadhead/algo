78e2a7d991074cf22a21328339519ead6999b0a3
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-10-23 21:06:33 +0900

    [level 3] Title: 합승 택시 요금, Time: 180.97 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -3,50 +3,51 @@ import java.util.*;
 
 class Solution {
     static ArrayList<Vertex> [] lists;
-
+    static int N;
     public int solution(int n, int s, int a, int b, int[][] fares) {
+        // 0. initialize values;
+        N = n;
         lists = new ArrayList [n+1];
-        
-        for(int i = 1; i < lists.length; i++){
+        for(int i = 0; i < n+1; i++){
             lists[i] = new ArrayList<>();
         }
         
-        for(int i = 0; i <fares.length; i++){
+        for(int i = 0; i < fares.length; i++){
             int start = fares[i][0];
-            int end = fares[i][1];
+            int end   = fares[i][1];
             int weight = fares[i][2];
+            
             lists[start].add(new Vertex(end, weight));
-            lists[end].add(new Vertex(start, weight));
+            lists[end].add(new Vertex(start,weight));
         }
         
-        int [] togther = dijstra(s);
-        int ans = togther[a] + togther[b]; // 따로 가는 것 
-        for(int i = 1; i <= n; i++){            
-            int [] alone = dijstra(i);
-            ans = Math.min(ans, togther[i]+alone[a]+alone[b]);
+        int [] together = dijstra(s);   // 시작 정점에서 모든 정점까지의 최단거리 배열
+        int ans = Integer.MAX_VALUE;    
+        for(int i = 1; i < n+1; i++){   // 경유지 찾기
+            int [] alone = dijstra(i);  // 경유지에서 모든 정점까지의 최단거리 배열  
+            ans = Math.min(ans, together[i] + alone[a] + alone[b]);
         }
+        
         return ans;
         
     }
     
-    public int [] dijstra(int start){
-        PriorityQueue<Vertex> pq = new PriorityQueue<>((o1,o2) -> o1.cost - o2.cost);
-        int [] dist = new int [lists.length];
+    public int[] dijstra(int start) {
+        int [] dist = new int[N+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
+        PriorityQueue<Vertex> pq = new PriorityQueue<>((o1,o2) -> o1.cost -o2.cost);
         pq.add(new Vertex(start, 0));
         dist[start] = 0;
         
         while(!pq.isEmpty()){
             Vertex now = pq.poll();
-            if(now.cost > dist[now.idx]) continue; 
-          
+            if(now.cost > dist[now.idx]) continue;
+            
             for(int i = 0; i < lists[now.idx].size(); i++){
-                int nv = lists[now.idx].get(i).idx;
-                int nDist = dist[now.idx] + lists[now.idx].get(i).cost; // (현재 정점까지의 최단거리 + 현 정점에서 nv까지의 간선 비용)
-                
-                if(dist[nv] > nDist){
-                    dist[nv] = nDist;
-                    pq.add(new Vertex(nv, nDist));
+                Vertex next = lists[now.idx].get(i);
+                if(next.cost + dist[now.idx] < dist[next.idx]){
+                    dist[next.idx] = next.cost + dist[now.idx];
+                    pq.add(new Vertex(next.idx, dist[next.idx]));
                 }
             }
         }
@@ -55,12 +56,11 @@ class Solution {
     }
 }
 
-class Vertex {
+class Vertex{
     int idx;
     int cost;
-
     
-    public Vertex (int idx, int cost){
+    public Vertex(int idx, int cost){
         this.idx = idx;
         this.cost = cost;
     }
