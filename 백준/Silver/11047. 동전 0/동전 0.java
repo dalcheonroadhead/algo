5a7036f8c587ff933dc44d268be78c24885b4607
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-11-08 00:11:09 +0900

    [Silver IV] Title: 동전 0, Time: 108 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int [] units = new int [N];
+
+        for(int i = 0; i < N; i++){
+            units[i] = Integer.parseInt(br.readLine());
+        }
+
+        int ans = 0;
+        for(int i = N-1; i >= 0; i--){
+            if(K/units[i] != 0) {
+                ans += K/units[i];
+                K -= (K/units[i])*units[i];
+            }
+            if(K == 0) break;
+        }
+        System.out.println(ans);
+    }
+}
