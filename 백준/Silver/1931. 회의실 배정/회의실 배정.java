255523f9e009516c07fbcdf34f4b5d00357ad355
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-11-09 14:48:54 +0900

    [Silver I] Title: 회의실 배정, Time: 472 ms, Memory: 43260 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,91 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    static int N, first, last;
-    static int max = 0;
-    static MeetingHour [] Meetings;
-
-    static boolean [] flag;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int [][] lecture = new int [N][2];
 
-        N = Integer.parseInt(br.readLine());
-
-        Meetings = new MeetingHour[N];
-        flag = new boolean[N];
-
-        for (int i = 0; i < N; i++) {
+        for(int i = 0; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            Meetings[i] = new MeetingHour(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            lecture[i][0] = Integer.parseInt(st.nextToken());
+            lecture[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(Meetings);
-        Combination(0);
+        Arrays.sort(lecture, (a,b) -> {
+            if(a[1] == b[1]) return a[0] - b[0];
+            return Integer.compare(a[1], b[1]);
+        });
 
-        System.out.println(max);
-    }
+        int end_time = lecture[0][1];
+        int cnt = 1;
+        for(int i = 1; i < N; i++){
+            if(lecture[i][0] < end_time) continue;
 
+            cnt++;
+            end_time = lecture[i][1];
 
-    public static void Combination (int deepth) {
-
-        if(deepth == N) {
-            int cnt = 0;
-            for (int i = 0; i < flag.length; i++) {
-                if(flag[i]) cnt++;
-            }
-
-            max = Math.max(cnt, max);
-            first = 0;
-            last = 0;
-            return;
         }
-
-
-
-            if(deepth == 0) {
-                flag[deepth] = true;
-                first = Meetings[deepth].start;
-                last = Meetings[deepth].end;
-                Combination(deepth+1);
-                flag[deepth] = false;
-            }
-
-             if(Meetings[deepth].start >=last){
-                flag[deepth]= true;
-                last = Meetings[deepth].end;
-                Combination(deepth+1);
-
-            }else {
-                Combination(deepth+1);
-            }
-    }
-
-}
-
-class MeetingHour implements Comparable<MeetingHour>{
-    int start;
-    int end;
-
-    public MeetingHour(int start, int end) {
-        this.start = start;
-        this.end = end;
-    }
-
-    @Override
-    public int compareTo(MeetingHour o) {
-
-        if(this.end == o.end){
-            return this.start-o.start;
-        }
-        return this.end-o.end;
+        System.out.println(cnt);
     }
 }
