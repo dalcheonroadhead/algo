29212dbfaaca5751088f9853db2e70576be848c8
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-01-02 00:43:54 +0900

    [Gold II] Title: 버블 소트, Time: 1596 ms, Memory: 73632 KB -BaekjoonHub

--- 백준/Gold/1377. 버블 소트/버블 소트.java
+++ 백준/Gold/1377. 버블 소트/버블 소트.java
@@ -1,37 +1,40 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.StringTokenizer;
+
 
 public class Main {
-
+    /*
+    * 1377 버블 소트
+    * (1) 모든 원소의 원래 index를 저장
+    * (2) 모든 원소의 정렬 후 index를 구한다.
+    * (3) (1) - (2) 중 최대값을 구한다.
+    * (4) 최대값 + 1을 출력한다.
+    * */
     public static void main(String[] args) throws IOException {
+        int answer = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        Node [] nodes = new Node[N];
-
+        Num [] nums = new Num[N];
         for (int i = 0; i < N; i++) {
-            Node now = new Node(i, Integer.parseInt(br.readLine()));
-            nodes[i] = now;
+            nums[i] = new Num(i, Integer.parseInt(br.readLine()));
         }
-
-        Arrays.sort(nodes, Comparator.comparingInt(o -> o.value));
-
-        int max = 0;
+        Arrays.sort(nums, Comparator.comparingInt(o -> o.v));
         for (int i = 0; i < N; i++) {
-            max = Math.max(max, nodes[i].idx - i);
+            if(i < nums[i].i) answer = Math.max(answer, nums[i].i - i);
         }
-
-        System.out.println(++max);
+        System.out.println(answer+1);
     }
 }
 
-class Node {
-    int idx;
-    int value;
+class Num {
+    int i;
+    int v;
 
-    public Node(int idx, int value) {
-        this.idx = idx;
-        this.value = value;
+    public Num(int i, int v){
+        this.i = i;
+        this.v = v;
     }
 }
