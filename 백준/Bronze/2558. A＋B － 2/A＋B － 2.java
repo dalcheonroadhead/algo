a09a5d8e3d8a02f7c7741e6b88b34d4b0fbda755
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-01-02 16:25:37 +0900

    [Bronze V] Title: A+B - 2, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        System.out.println(A + B);
+    }
+}
