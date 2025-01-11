b2fa462e47a469c160ac57f1894ab18c808af19d
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-01-11 17:51:37 +0900

    [Bronze V] Title: 웰컴, Time: 100 ms, Memory: 13860 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder ans = new StringBuilder();
+        ans.append(".  .   .").append("\n");
+        ans.append("|  | _ | _. _ ._ _  _").append("\n");
+        ans.append("|/\\|(/.|(_.(_)[ | )(/.").append("\n");
+        System.out.println(ans);
+    }
+}
