82e903c988be85c13868f0865f2e8b724e91dbab
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-01-13 13:35:18 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 92 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder ans = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        ans.append("       _.-;;-._").append("\n");
+        ans.append("'-..-'|   ||   |").append("\n");
+        ans.append("'-..-'|_.-;;-._|").append("\n");
+        ans.append("'-..-'|   ||   |").append("\n");
+        ans.append("'-..-'|_.-''-._|");
+        System.out.println(ans.toString());
+    }
+
+}
