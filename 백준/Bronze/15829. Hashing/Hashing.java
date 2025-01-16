a6f158d86c5b8b8ae124509f8cb29d6761259c2b
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-01-16 15:05:29 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14208 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -3,20 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    private static final long M = 1234567891;
+    private static final long r = 31;
     public static void main(String[] args) throws IOException {
         int N;
         String str;
         long sum = 0;
-        long  ans = 0;
+        long  ans;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         str = br.readLine();
 
         for (int i = 0; i < N; i++) {
             int now = str.charAt(i) - 'a' + 1;
-            sum += ((long)now * (long)Math.pow(31,i));
+            long ri = 1;
+            for (int j = 0; j < i; j++) {
+                ri *=r;
+                ri %=M;
+            }
+            sum += ((long)now * ri)%M;
         }
-        ans = (sum % 1234567891);
+        ans = (sum % M);
         System.out.println(ans);
     }
 }
