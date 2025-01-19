b7f13a46596b32cc1817ab303e57c3ac1089c0d5
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-01-19 23:07:15 +0900

    [level 2] Title: 요격 시스템, Time: 461.76 ms, Memory: 195 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/README.md
+++ 프로그래머스/2/181188. 요격 시스템/README.md
@@ -1,10 +1,10 @@
 # [level 2] 요격 시스템 - 181188 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181188) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181188#) 
 
 ### 성능 요약
 
-메모리: 188 MB, 시간: 417.25 ms
+메모리: 195 MB, 시간: 461.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 12일 01:10:36
+2025년 01월 19일 23:07:10
 
 ### 문제 설명
 
--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -4,22 +4,17 @@ import java.util.*;
 class Solution {
     
     public int solution(int[][] targets) {
+        int ans = 0;
         Arrays.sort(targets, (o1,o2) -> {
-            if(o1[1] == o2[1]) return o1[0] - o2[0];
+            if(o1[1]==o2[1]) return o1[0] - o2[0];
             return o1[1] - o2[1];
         });
         
-        int ans = 0;
-        int pos = 0;
-        
         for(int i = 0; i < targets.length; i++){
-            if(targets[i][0]>= pos){
-                ans++;
-                pos = targets[i][1];
-            }
+            int shot = targets[i][1];
+            while(i < targets.length - 1 && shot > targets[i+1][0]) i++;
+            ans++;
         }
-        
         return ans;
-        
     }
 }
