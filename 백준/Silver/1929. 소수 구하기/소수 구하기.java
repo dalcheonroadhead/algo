49bef39e29b51039306df597c6d4b2766b39b4ec
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-01-20 16:29:35 +0900

    [Silver III] Title: 소수 구하기, Time: 232 ms, Memory: 20396 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,30 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+
 public class Main {
+    static int N, M;
+    static boolean [] nums;
+    static StringBuilder ans = new StringBuilder();
     public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nums = new boolean [M + 1];
+        prime_shifter();
+        for (int i = N; i <= M; i++) {
+            if(nums[i]) ans.append(i).append("\n");
+        }
+        System.out.println(ans);
+    }
 
-            int M = Integer.parseInt(st.nextToken());
-            int N = Integer.parseInt(st.nextToken());
-
-            boolean [] bb = new boolean[N+1];
-            bb[1] = bb[0] = true;
-
-        for (int i = 2; i < Math.sqrt(N+1); i++) {
-            for (int j = i*i; j < N+1; j+=i) {
-                bb[j] = true;
+    public static void prime_shifter(){
+        Arrays.fill(nums, true);
+        nums[1] = false;
+        nums[0] = false;
+        for (int i = 2; i <= Math.sqrt(M); i++) {
+            if(nums[i]){
+                for (int j = i*i; j <= M; j+=i) {
+                    nums[j] = false;
+                }
             }
         }
-
-        for (int i = M; i < N+1; i++) {
-            if(!bb[i]){
-                System.out.println(i);
-            }
-        }
-
     }
 }
