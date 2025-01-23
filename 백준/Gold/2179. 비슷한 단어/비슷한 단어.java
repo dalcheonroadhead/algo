be0a54721fc7daf6bd5ef3ac3f959bf969dc0ae8
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-01-23 16:22:50 +0900

    [Gold IV] Title: 비슷한 단어, Time: 192 ms, Memory: 28056 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -10,46 +10,66 @@ import java.util.TreeMap;
 
 
 public class Main {
-    static int N;
-    static String [] strings;
-    static int max_cnt = 0;
-    static String S, T;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        strings = new String[N];
-        for(int i = 0; i < N; i++){
-            strings[i] = br.readLine();
-        }
-        combination();
-        System.out.println(S);
-        System.out.println(T);
-    }
-    public static void combination(){
-        for (int i = 0; i < N-1; i++) {
-            String A = strings[i];
-            for (int j = i+1; j < N; j++) {
-                String B = strings[j];
-                if(max_cnt > B.length()) continue;
-                int now_cnt =same_cnt(A,B);
-                if(max_cnt < now_cnt){
-                    max_cnt = now_cnt;
-                    S = A;
-                    T = B;
-                }
-            }
+    private static class Node {
+        HashMap<Character,Node> children;
+        int word_idx;
+
+        public Node(int idx){
+            children = new HashMap<>();
+            this.word_idx = idx;
         }
     }
 
-    public static int same_cnt (String str1, String str2){
-        int length = Math.min(str1.length(), str2.length());
-        int cnt = 0;
-        for (int i = 0; i < length; i++) {
-            char c1 = str1.charAt(i);
-            char c2 = str2.charAt(i);
-            if(c1 != c2) break;
-            cnt++;
+    private static class Tri {
+        Node root = new Node(-1);
+
+        public void insert(String str, int word_idx) {
+            Node now = this.root;
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                if(!now.children.containsKey(c)) now.children.put(c, new Node(word_idx));
+                else {
+                    // 현재 들어온 tri의 깊이가 지금까지의 최대값보다 크다.
+                    // 현재 확인하려는 Node(다음 Node)를 저장시켰던 단어가 현재 보는 단어랑 같지 않다.
+                    if(i >= Max && !str.equals(Words[now.children.get(c).word_idx])) {
+                        if(i > Max) {
+                            Max = i;
+                            ans[0] = now.children.get(c).word_idx;
+                            ans[1] = word_idx;
+                        } else if (i == Max) {
+                            if(now.children.get(c).word_idx < ans[0]) {
+                                ans[0] = now.children.get(c).word_idx;
+                                ans[1] = word_idx;
+                            }
+                        }
+                    }
+                }
+                now = now.children.get(c);
+            }
+
         }
-        return cnt;
+    }
+
+    private static int [] ans = new int [2];
+    private static String [] Words;
+    private static int Max = -1;
+    private static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        Words = new String[N];
+        Tri tri = new Tri();
+        for (int i = 0; i < N; i++) {
+            Words[i] = br.readLine();
+            tri.insert(Words[i], i);
+        }
+
+        if(Max == -1) {
+            ans[0] = 0;
+            ans[1] = 1;
+        }
+
+        System.out.println(Words[ans[0]]);
+        System.out.println(Words[ans[1]]);
     }
 }
