92e2f9601a65405ed5acf3f0b8707dc847c91465
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-02-10 00:48:12 +0900

    [Bronze V] Title: 연세대학교, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        Solution solution = new Solution();
+//        solution.solution();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        if (N == 0) {
+            System.out.println("YONSEI");
+        }else {
+            System.out.println("Leading the Way to the Future");
+        }
+
+    }
+}
