b5029b04456ad95a139cf6c0d9fca221da77389a
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-05-03 14:51:44 +0900

    [Gold III] Title: 줄 세우기, Time: 420 ms, Memory: 43172 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -5,13 +5,9 @@ public class Main{
     static int N, M;
     static ArrayList<Integer> [] lists;
     static int [] inDegree;
-    static ArrayList<Integer> ans = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         init();
         topologicalSorting();
-        for(int temp : ans){
-            System.out.print(temp + " ");
-        }
     }
 
     public static void init() throws IOException {
@@ -34,19 +30,25 @@ public class Main{
         }
     }
     public static void topologicalSorting(){
-        while(ans.size() < N){
-            int now = 0;
-            for (int i = 1; i < N+1; i++) {
-                if(inDegree[i] == 0) {
-                    inDegree[i]--;
-                    now = i;
-                    break;
-                }
-            }
-            ans.add(now);
-            for (int i = 0; i < lists[now].size(); i++) {
-                inDegree[lists[now].get(i)]--;
+        StringBuilder ans = new StringBuilder();
+        ArrayDeque<Integer> aq1 = new ArrayDeque<>();
+        for (int i = 1; i < N+1; i++) {
+            if(inDegree[i] == 0) {
+                aq1.add(i);
+                ans.append(i).append(" ");
             }
         }
+        while (!aq1.isEmpty()){
+            int now = aq1.poll();
+            for (int i = 0; i < lists[now].size(); i++) {
+                int next = lists[now].get(i);
+                inDegree[next]--;
+                if(inDegree[next] == 0) {
+                    aq1.add(next);
+                    ans.append(next).append(" ");
+                }
+            }
+        }
+        System.out.println(ans);
     }
 }
