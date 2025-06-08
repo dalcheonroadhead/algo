c1c1c570c7a6ce4590ae7b6f0c15cbba361aa03c
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-06-09 01:48:30 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 87.99 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,38 +1,19 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        
         HashMap<String, Integer> map = new HashMap<>();
-        
-        for(int i = 0; i < participant.length; i ++){
-            if(map.putIfAbsent(participant[i], 1) != null){
-                map.put(participant[i], map.get(participant[i]) + 1);
-            }
+        for(String now : participant){
+            map.compute(now, (k,ov)-> ov==null? 1 : ov+1);
         }
         
-        for(int i = 0; i < completion.length; i ++){
-            if(map.get(completion[i]) == null){
-                return completion[i];
-            }else {
-                map.put(completion[i], map.get(completion[i])-1);
-            }
+        for(String now : completion) {
+            map.compute(now, (k,ov) -> ov-1  <= 0? null : ov-1);
         }
         
-        for(int i = 0; i < map.size(); i ++){
-            if(map.get(participant[i]) > 0){
-                return participant[i];
-            }
+        for(String now : map.keySet()) {
+            return now;
         }
-        
-
-        
-        
-
-        
-        
-        return "여기까지 안 옴";
-        
+        return "";    
     }
 }
