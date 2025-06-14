4f6d659c5dddffb8ac5df028e1d9e38562982416
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-06-14 22:29:45 +0900

    [Silver III] Title: N과 M (2), Time: 112 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,43 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int[] list;
-	static int N,M;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-			// 자연수가 1부터 ~N 
-		  N = Integer.parseInt(st.nextToken());
-		  
-		  M = Integer.parseInt(st.nextToken());
-		 
-		  // 조합으로 선택된 값들이 담길 곳, 
-		  list = new int[M];
-		  Combination(0, 1);
-		
-	}
-	
-	public static void Combination(int deepth, int start) {
-		if(deepth == M) {
-			for(int a : list) {
-				System.out.println(a);
-			}
-			return;
-		}
-		
-		for(int i = start; i <= N ; i ++) {
-			list[deepth] = i; 
-			Combination(deepth+1, i+1);
-		}
-		
-	}
+    static StringBuilder sb = new StringBuilder();
+    static int cnt = 0;
+    static int n,r;
+    static boolean [] isSelected;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        isSelected = new boolean [n+1];
+        combination(1, 0);
+        System.out.println(sb);
+    }
+
+    public static void combination(int start, int depth) {
+        // base-case
+        if(depth == r){
+            for(int i = 1; i < n+1; i++) {
+                if(isSelected[i]) sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start; i < n+1; i++){
+            isSelected[i] = true;
+            combination(i+1, depth+1);
+            isSelected[i] = false;
+        }
+    }
 }
