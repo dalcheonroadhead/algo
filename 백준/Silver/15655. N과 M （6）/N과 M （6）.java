efe33942c7e76ea787f440afc8d81c30fbe860f7
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-06-14 23:24:08 +0900

    [Silver III] Title: N과 M (6), Time: 112 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int cnt = 0;
+    static int n,r;
+    static int [] arr;
+    static boolean [] isVisited;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        arr = new int [n];
+        isVisited = new boolean [n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        combination(0,0, new int [r]);
+        System.out.println(sb);
+    }
+
+    public static void permutation (int depth, int [] answer) {
+        // base-case
+        if(depth == r) {
+            for(int temp : answer){
+                sb.append(temp).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0; i < n; i++){
+            if(isVisited[i]) continue;
+            isVisited[i] = true;
+            answer[depth] = arr[i];
+            permutation(depth+1, answer);
+            isVisited[i] = false;
+        }
+    }
+
+    public static void combination(int start, int depth, int [] answer) {
+        // base-case
+        if(depth == r) {
+            for(int now : answer){
+                sb.append(now).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start; i < n; i++){
+            answer[depth] = arr[i];
+            combination(i+1, depth+1, answer);
+        }
+    }
+}
