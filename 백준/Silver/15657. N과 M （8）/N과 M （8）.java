aba620e551492a8b42de0329720f86780087323a
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-06-14 23:39:35 +0900

    [Silver III] Title: N과 M (8), Time: 132 ms, Memory: 17040 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int cnt = 0;
+    static int n,r;
+    static int [] arr;
+    static boolean [] isVisited;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        arr = new int [n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        duple_combination(0, 0, new int [r]);
+        System.out.println(sb);
+    }
+
+    public static void duple_permutation (int depth, int [] answer) {
+        // base-case
+        if(depth == r) {
+            for(int temp : answer){
+                sb.append(temp).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0; i < n; i++){
+            answer[depth] = arr[i];
+            duple_permutation(depth+1, answer);
+        }
+    }
+
+    public static void duple_combination(int start, int depth, int [] answer) {
+        // base-case
+        if(depth == r) {
+            for(int now : answer){
+                sb.append(now).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start; i < n; i++){
+            answer[depth] = arr[i];
+            duple_combination(i, depth+1, answer);
+        }
+    }
+}
