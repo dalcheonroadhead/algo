5daef52e0e71088e1bddfc2d96817638f8847382
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-06-14 22:14:32 +0900

    [Silver III] Title: N과 M (1), Time: 240 ms, Memory: 22220 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -2,50 +2,40 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-    // N은 내가 가용할 수 있는 수
-    // M은 내가 나열할 숫자의 수
-    static int N,M;
-    static boolean [] isVisted;
-    static int [] arr;
-
+    static int n,r;
+    static boolean [] isVisited;
     static StringBuilder sb = new StringBuilder();
-
-
-    public static void main(String[] args) throws IOException {
-
-        // 값 입력 받기
+    
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        isVisted = new boolean[N];
-        arr = new int[M];
-        cal(0);
-
+        n = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        isVisited = new boolean [n+1];
+        permutation(0, new int [n]);
         System.out.println(sb);
+        
     }
 
-    public static void cal(int deepth) {
-
-        if(deepth == M){
-            for (int i = 0; i < arr.length; i++) {
-                sb.append(arr[i]).append(" ");
+    public static void permutation(int depth, int [] answer) {
+        // base-case
+        if(depth == r) {
+            for(int i = 0; i < n; i++){
+                if(answer[i] == 0) continue;
+                sb.append(answer[i]).append(" ");
+                
             }
             sb.append("\n");
             return;
         }
 
+        for(int i = 1; i < n+1; i++){
+            if(isVisited[i]) continue;
 
-        for (int i = 0; i < N; i++) {
-            if(!isVisted[i]){
-                arr[deepth] = i+1;
-                isVisted[i] = true;
-                cal(deepth+1);
-                isVisted[i] = false;
-            }
+            isVisited[i] = true;
+            answer[depth] = i; 
+            permutation(depth+1, answer);
+            isVisited[i] = false;
         }
     }
 }
