854c3cdc5125701dac54b0e145a834dfab2b9537
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-06-26 16:26:51 +0900

    [Gold V] Title: 선발 명단, Time: 148 ms, Memory: 15888 KB -BaekjoonHub

--- 백준/Gold/3980. 선발 명단/선발 명단.java
+++ 백준/Gold/3980. 선발 명단/선발 명단.java
@@ -2,49 +2,51 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int answer = 0;
-    static int [][] members;
-    static boolean [] isVisited;
+    // 포지션 별로 멤버 넣기 (멤버가 해당 포지션에서 능력치가 0이면 원래 재귀로 복귀 - 백 트래킹)
+    // 다 넣으면 계산해서 최대값 갱신
+
+    static int [][] map; // 행 = 선수, 열 = 포지션
+    static int [] position;
+    static boolean [] check;
+    static int max;
+    
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int TC = Integer.parseInt(br.readLine());
 
         for(int t = 0; t < TC; t++){
-            members = new int [11][11];
+            max = 0;
+            map = new int[11][11];
+            position = new int [11];
+            check = new boolean [11];
             for(int i = 0; i < 11; i++){
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 for(int j = 0; j < 11; j++){
-                    members[i][j] = Integer.parseInt(st.nextToken());
+                    map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            isVisited = new boolean [11];
-            permutation(0, new int [11]);
-            System.out.println(answer);
-            answer=0;
+            permu(0);
+            System.out.println(max);
         }
     }
-    // members의 행 = 선수 번호, 열은 포지션
-    // answer의 index = 선수 번호, answer의 value = 포지션
-    // isVisited의 index = 포지션, value = 포지션 썼는지 아닌지
-    public static void permutation(int depth, int [] position) {
-        // base-case
+
+    public static void permu (int depth) {
         if(depth == 11) {
             int sum = 0;
             for(int i = 0; i < 11; i++){
-                sum += members[i][position[i]];
+                sum += position[i];
             }
-            answer = Math.max(answer, sum);
+            max = Math.max(max, sum);
             return;
         }
 
-        for(int i = 0; i < 11; i++) {
-            if(isVisited[i]) continue;
-            if(members[depth][i] == 0) continue;
-
-            isVisited[i] = true;
-            position[depth] = i;
-            permutation(depth+1, position);
-            isVisited[i] = false;
+        for(int i = 0; i < 11; i++){
+            if(check[i]) continue;
+            if(map[depth][i] == 0) continue;
+            check[i] = true;
+            position[depth] = map[depth][i];
+            permu(depth+1);
+            check[i] = false;
         }
     }
 }
