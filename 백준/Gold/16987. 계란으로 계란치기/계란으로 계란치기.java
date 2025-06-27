42f2188f41b60e266f1b18b5653d64a181b6d218
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-06-27 16:27:53 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 184 ms, Memory: 15196 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -2,59 +2,64 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+
+    static class Egg {
+        int hp;
+        int w;
+
+        public Egg(int hp, int w){
+            this.hp = hp;
+            this.w = w;
+        }
+
+        @Override
+        public String toString() {
+            return "체력:"+ this.hp + "\t 무게: " + this.w;
+        }
+    }
     static int N;
-    static int [][] arr;
     static int ans = 0;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        arr = new int [N][2];
+        Egg [] eggs = new Egg[N];
+
         for(int i = 0; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+            int hp = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            eggs[i] = new Egg(hp, weight);
         }
-        permu(0, new int [N]);
+
+        recur(0, eggs, 0);
         System.out.println(ans);
     }
-    // order == (index = 치는 계란, value = 맞는 계란)
-    private static void permu (int depth, int [] order){
+
+    public static void recur (int depth, Egg [] eggs, int cnt){
+        ans = Math.max(ans,cnt);
         if(depth == N) {
-            ans = Math.max(ans, calculate(order));
             return;
         }
-
-        for(int i = 0 ; i < N; i++){
-            if(depth == i) continue;
-            order[depth] = i;
-            permu(depth +1, order);
+        // 이미 깨진 계란이면 다음 계란으로 넘어간다.
+        if(eggs[depth].hp <= 0) {
+            recur(depth+1, eggs, cnt);
+            return;
         }
-    }
+        for(int i = 0; i < N; i++){
+            if(i == depth) continue;
+            if(eggs[i].hp <= 0) continue;
 
-    private static int calculate (int [] order) {
-        int cnt = 0;
-        int [][] egg = new int [arr.length][2];
+            int new_cnt = cnt;
+            eggs[i].hp -= eggs[depth].w;
+            eggs[depth].hp -= eggs[i].w;
 
-        for(int i = 0; i < arr.length; i++){
-            egg[i][0] = arr[i][0];
-            egg[i][1] = arr[i][1];
+            if(eggs[i].hp <= 0) new_cnt++;
+            if(eggs[depth].hp <= 0) new_cnt++;
+
+            recur(depth+1, eggs, new_cnt);
+            eggs[i].hp += eggs[depth].w;
+            eggs[depth].hp += eggs[i].w;
         }
-
-        for(int i = 0; i < order.length; i++){
-            int hitting = i;
-            int hitted = order[i];
-            if(egg[hitting][0] <= 0 || egg[hitted][0] <= 0) continue;
-
-            egg[hitting][0] -= egg[hitted][1];
-            egg[hitted][0] -= egg[hitting][1];
-
-            if(egg[hitting][0] <= 0) {
-                cnt++;
-            }
-            if(egg[hitted][0] <= 0) {
-                cnt++;
-            }
-        }
-        return cnt;
     }
 }
