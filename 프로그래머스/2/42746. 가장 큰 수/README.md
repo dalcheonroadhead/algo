439148d10d34813bd4b91a104e80bc15c639409f
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-07-10 20:35:51 +0900

    [level 2] Title: 가장 큰 수, Time: 407.39 ms, Memory: 409 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746#) 
 
 ### 성능 요약
 
-메모리: 159 MB, 시간: 274.28 ms
+메모리: 409 MB, 시간: 407.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 01일 16:00:52
+2025년 07월 10일 20:35:48
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,45 +1,29 @@
 import java.util.*;
 
-
 class Solution {
     public String solution(int[] numbers) {
-        Integer [] arr = new Integer [numbers.length];
-        
-        for(int i = 0; i < numbers.length; i++){
-            arr[i] = numbers[i];
+        Integer [] nums = new Integer [numbers.length];
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = numbers[i];
         }
         
-        Arrays.sort(arr, (a, b) -> {
-            String sA = Integer.toString(a);
-            String sB = Integer.toString(b);
-            
-            String temp1 = sA+sB;
-            String temp2 = sB+sA;
-            
-            int ab = Integer.parseInt(temp1);
-            int ba = Integer.parseInt(temp2);
-            
-            if(ab > ba) return -1;
-            else if (ab == ba) return 0;
-            else return 1;
-        });
+        Arrays.sort(nums,             
+            (a,b) -> {
+                StringBuilder candi1 = new StringBuilder();
+                StringBuilder candi2 = new StringBuilder();
+                candi1.append(a).append(b);
+                candi2.append(b).append(a);
+                return Integer.parseInt(candi2.toString()) - Integer.parseInt(candi1.toString());
+            });
         
-        return join(arr);
-    }
-    
-    public String join (Integer [] arr) {
-        StringBuilder sb = new StringBuilder();
-        int zero_cnt = 0;
-        
-        for(Integer i : arr){
-            if(i == 0) zero_cnt++;
-            sb.append(i);
+        StringBuilder answer = new StringBuilder();
+        for(int temp : nums){
+            answer.append(temp);
         }
-        if(zero_cnt == arr.length) {
-            sb.setLength(0);
-            sb.append("0");
+        if(answer.charAt(0) == '0') {
+            answer.setLength(0);
+            answer.append(0);
         }
-        
-        return sb.toString();
+        return answer.toString();
     }
 }
