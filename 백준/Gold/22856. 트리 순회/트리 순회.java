cb097694686f6aa5ec505bcd81793dcab6662522
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-07-17 22:16:17 +0900

    [Gold IV] Title: 트리 순회, Time: 492 ms, Memory: 62532 KB -BaekjoonHub

--- 백준/Gold/22856. 트리 순회/트리 순회.java
+++ 백준/Gold/22856. 트리 순회/트리 순회.java
@@ -1,53 +1,44 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-    static int N, last_one, cnt;
-    static int answer = 0;
-    static ArrayList<Integer> [] lists;
-    static boolean [] check;
+    static int N;
+    static ArrayList<Integer>[] lists;
+    static int lastNode; // 중위순회 마지막 노드 번호
+    static int lastDepth; // 마지막 노드까지의 depth
+
     public static void main(String[] args) throws IOException {
         init();
-        inorder_traversal(1);
-        similar_inorder(1);
-        System.out.printf("%d", answer);
+        findLastNode(1);          // 1. 중위순회로 마지막 노드 찾기
+        findDepth(1, 0);          // 2. 마지막 노드까지의 depth 탐색
+
+        int edgeCount = N - 1;    // 3. 전체 간선 개수
+        int answer = edgeCount * 2 - lastDepth;
+        System.out.println(answer);
     }
 
-    public static void inorder_traversal(int now) {
-        if(lists[now].get(0) != -1) inorder_traversal(lists[now].get(0));
-        last_one = now;
-        if(lists[now].get(1) != -1) inorder_traversal(lists[now].get(1));
+    // 중위순회로 마지막 노드 발견
+    static void findLastNode(int now) {
+        if (lists[now].get(0) != -1) findLastNode(lists[now].get(0));
+        lastNode = now;
+        if (lists[now].get(1) != -1) findLastNode(lists[now].get(1));
     }
 
-    public static void similar_inorder(int now) {
-        if(lists[now].get(0) != -1 && !check[lists[now].get(0)]) {
-            cnt++;
-            similar_inorder(lists[now].get(0));
-            cnt++;
-        }
-        if(lists[now].get(1) != -1 && !check[lists[now].get(1)]) {
-            cnt++;
-            similar_inorder(lists[now].get(1));
-            cnt++;
-        }
-        if(now == last_one) {
-            answer = cnt;
-            return;
-        } else check[now] = true;
+    // DFS로 lastNode까지의 거리(depth) 측정
+    static void findDepth(int now, int depth) {
+        if (now == -1) return;
+        if (now == lastNode) lastDepth = depth;
+        if (lists[now].get(0) != -1) findDepth(lists[now].get(0), depth + 1);
+        if (lists[now].get(1) != -1) findDepth(lists[now].get(1), depth + 1);
     }
-
 
     public static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         lists = new ArrayList[N+1];
-        check = new boolean[N+1];
 
-        for(int i = 1; i <= N; i++){
+        for(int i = 1; i <= N; i++)
             lists[i] = new ArrayList<>();
-        }
-
         for(int i = 0; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int me = Integer.parseInt(st.nextToken());
