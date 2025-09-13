2ba555313b1b00c3606d19e45a908cb4bb7d397f
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-09-13 15:35:05 +0900

    [Gold V] Title: 거의 소수, Time: 380 ms, Memory: 92420 KB -BaekjoonHub

--- 백준/Gold/1456. 거의 소수/거의 소수.java
+++ 백준/Gold/1456. 거의 소수/거의 소수.java
@@ -1,47 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-
+// 1456 거의 소수
 public class Main {
-    static long  A, B;
-    static long [] nums;        // 0 = true, 1 = false
+    static Long A, B;
+    static long [] nums = new long [(int) Math.pow(10,7) + 1]; // 0 = prime
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         A = Long.parseLong(st.nextToken());
         B = Long.parseLong(st.nextToken());
-        nums = new long [(int)Math.sqrt(B) + 1];
-        prime_shifter();
-        System.out.println(counting_almost_prime());
+        sieve();
+        int sum = 0;
+        for (long i = 2; i < nums.length; i++) {
+            if(nums[(int)i] == 1) continue;   // 소수가 아니면 넘어감
+            long temp = i * i;
+            while (temp <= B){
+                if(temp >= A) sum++;
+                if(temp > (B/i)) break;
+                temp *= i;
+            }
+        }
+        System.out.println(sum);
     }
 
-    public static void prime_shifter(){
+    public static void sieve () {
         nums[0] = 1;
         nums[1] = 1;
-        for (int i = 2; i <= Math.sqrt(Math.sqrt(B)); i++) {
-            if(nums[i] == 0){
-                for (int j = i*i; j <= Math.sqrt(B); j+=i) {
-                    nums[j] = 1;
-                }
+        for (int i = 2; i < Math.sqrt(nums.length); i++) {
+            if(nums[i] == 1) continue;
+            for (int j = i*i; j < nums.length; j+=i) {
+                nums[j] = 1;
             }
         }
-    }
-
-    public static int counting_almost_prime(){
-        int cnt = 0;
-        for (long i = 2; i <= Math.sqrt(B); i++) {
-            if(nums[(int)i] == 0){
-                double j = i*i;
-                double acc = 2;
-                while(j <= B){
-                    if(j >= A) cnt++;
-                    j = Math.pow(i, ++acc);
-                }
-            }
-        }
-        return cnt;
     }
 }
