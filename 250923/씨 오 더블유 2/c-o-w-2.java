42c9c5df74f043d46a82e5d462a03dbfc748a74a
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-09-23 10:54:06 +0900

    Title: 씨 오 더블유 2, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250923/씨 오 더블유 2/c-o-w-2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String str = sc.next();
+        // Please write your code here.
+        int cnt = 0;
+        for(int i = 0 ; i < n-2; i++){
+            if(str.charAt(i) != 'C') continue;
+            for (int j = i+1; j < n-1; j++) {
+                if(str.charAt(j) != 'O') continue;
+                for(int k = j+1; k < n; k++) {
+                    if(str.charAt(k) != 'W') continue;
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
