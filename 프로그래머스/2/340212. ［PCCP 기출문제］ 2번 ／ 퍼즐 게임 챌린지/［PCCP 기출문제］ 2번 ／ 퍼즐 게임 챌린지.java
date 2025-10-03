67d8a3a404b71957a396c349ba9f0856b415483f
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-10-03 13:47:40 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 68.23 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,34 +1,57 @@
-import java.io.*;
-import java.util.*;
-// 1. 이분 탐색
-// 2. max = level 1로 문제 푼 시간, min = level 10000으로 문제 푼 시간, target = limit 값
-
-class Solution {    
+// 1. 이분 탐색 
+class Solution {
+    
+    static class Problem {
+        int d;
+        int t;
+        
+        public Problem (int d, int t){
+            this.d = d;
+            this.t = t;
+        }
+    }
+    static long L;
+    static Problem [] p;
+    
     public int solution(int[] diffs, int[] times, long limit) {
+        long answer = 0;
+        L = limit;
+        p = new Problem [diffs.length];
         
-        return binary_search(diffs, times, limit);
         
-    }
-    
-    public int binary_search(int [] diffs, int [] times, long limit) {
-        int max = 1; int min = 100000; 
-        while (max <= min){
-            int level = (max+min)/2; 
-            long mid = calcul(diffs,times, level);
-            // 제한 시간을 초과함 -> level 상승
-            if(mid > limit) max = level + 1; 
-            // 제한시간보다 빨리 끝내거나 딱 맞출 때 -> level 하락
-            else min = level - 1; 
-        }
-        return max;
-    }
-    
-    public long calcul(int [] diffs, int [] times, int level) {
-        long ans = 0;
         for(int i = 0; i < diffs.length; i++){
-            if(diffs[i] <= level) ans += times[i];
-            else ans += (long)(times[i] + times[i-1])* (long)(diffs[i] - level) + times[i];
+            p[i] = new Problem (diffs[i], times[i]);
         }
-        return ans;
+        answer = (int)binary_search();
+        System.out.println(answer);
+        return (int)answer;
+    }
+    
+    public long binary_search() {
+        long start = 1;
+        long end = 1_000_000_000_000_000L;
+        
+        while (start <= end) {
+            long mid = (start + end)/2;
+            if(!isOk(mid)) start = mid+1;
+            else end = mid-1;
+        }
+        return start;
+    }
+    
+    public boolean isOk (long level){ // 제한 시간을 넘니? 
+        
+        long sum = 0; // 실제 처리 시간
+        long prev = 0; // 이전까지의 시간을 단순 누적한 값
+        for(int i = 0 ; i < p.length; i++){
+            Problem now = p[i];
+            if(i != 0) prev = p[i].t + p[i-1].t; 
+            if(now.d <= level) sum += now.t;
+            else {
+                sum += now.t + (now.d - level)*prev;
+            }
+            // if(sum > L) return false; // 총 걸린 시간이 이미 limit을 넘었다. -> level을 올려야 함.
+        }
+        return sum > L? false : true; 
     }
 }
