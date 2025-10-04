659224010e0535b6e35f712de8b24e0969701a30
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-10-04 13:07:00 +0900

    Title: 최고의 13위치 2, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251004/최고의 13위치 2/best-place-of-13-2.java
+++ 251004/최고의 13위치 2/best-place-of-13-2.java
@@ -2,9 +2,10 @@ import java.util.*;
 
 public class Main {
     static int [][] acc;
+    static int n;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        n = sc.nextInt();
         int[][] arr = new int[n][n];
         acc = new int [n][n];
         for(int i = 0; i < n; i++)
@@ -31,7 +32,9 @@ public class Main {
         acc[r][c] = -99999;
         acc[r][c+1] = -99999;
         acc[r][c+2] = -99999999;
-
+        if(!OOB(r,c-1)) acc[r][c-1] = -99999;
+        if(!OOB(r,c-2)) acc[r][c-2] = -999999;
+        
         r = c = v = 0;
         for (int i = 0 ; i < n ; i++) {
             for (int j = 0; j < n; j++){
@@ -42,7 +45,10 @@ public class Main {
             }
         }
         answer += v;
-
         System.out.println(answer);
+    }
+
+    public static boolean OOB (int r, int c){
+        return r < 0 || c < 0 || r >= n || c >= n;
     }
 }
