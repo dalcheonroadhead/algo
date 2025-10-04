f320e78e9cbc1b6e817ca668e9a48b806f764b72
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2025-10-04 13:21:45 +0900

    Title: 최고의 13위치 2, Time: 136ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251004/최고의 13위치 2/best-place-of-13-2.java
+++ 251004/최고의 13위치 2/best-place-of-13-2.java
@@ -1,63 +1,47 @@
 import java.util.*;
 
 public class Main {
-    static int [][] acc;
+    static int [][] arr;
     static int n;
+    static int ans = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        int[][] arr = new int[n][n];
-        acc = new int [n][n];
+        arr = new int[n][n];
         for(int i = 0; i < n; i++)
             for(int j = 0; j < n; j++)
                 arr[i][j] = sc.nextInt();
         // Please write your code here.
-        int answer = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n-2; j++){
-                acc[i][j] = arr[i][j] + arr[i][j+1] + arr[i][j+2];
-            }
-        }
+        combination(0,0,0,new int[2][2], new boolean[n][n]);
+        System.out.println(ans);
+    }
 
-        // for(int [] row : acc){
-        //     System.out.println(Arrays.toString(row));
-        // }
-        // System.out.println();
-
-        int r,c,v;
-        r = c = v = 0;
-        for (int i = 0 ; i < n ; i++) {
-            for (int j = 0; j < n; j++){
-                if(v <= acc[i][j]) {
-                    v = acc[i][j];
-                    r = i; c = j;
-                }
-            }
+    public static void combination(int startR, int startC, int depth, int [][] chosen, boolean [][] check) {
+        if(depth == 2){
+            int firR = chosen[0][0];
+            int firC = chosen[0][1];
+            int secR = chosen[1][0];
+            int secC = chosen[1][1];
+            int sum = arr[firR][firC] + arr[firR][firC+1] + arr[firR][firC+2] + arr[secR][secC] + arr[secR][secC+1] + arr[secR][secC+2];
+            ans = Math.max(ans, sum);
+            return;
         }
-        answer += v;
-        acc[r][c] = -99999;
-        if(!OOB(r,c+1)) acc[r][c+1] = -99999;
-        if(!OOB(r,c+2)) acc[r][c+2] = -99999999;
-        if(!OOB(r,c-1)) acc[r][c-1] = -99999;
-        if(!OOB(r,c-2)) acc[r][c-2] = -999999;
         
+        for(int r = startR; r < n; r++){
+            for(int c = startC; c < n-2; c++){
+                if(check[r][c] || check[r][c+1] || check[r][c+2]) continue;
+                chosen[depth][0] = r;
+                chosen[depth][1] = c;
+                check[r][c] = true;
+                check[r][c+1] = true;
+                check[r][c+2] = true;
+                combination( (c == n-3)? r+1 : r, (c == n-3)? 0 : c+1, depth+1, chosen, check);
+                check[r][c] = false;
+                check[r][c+1] = false;
+                check[r][c+2] = false;
 
-        // for(int [] row : acc){
-        //     System.out.println(Arrays.toString(row));
-        // }
-
-
-        r = c = v = 0;
-        for (int i = 0 ; i < n ; i++) {
-            for (int j = 0; j < n; j++){
-                if(v < acc[i][j]) {
-                    v = acc[i][j];
-                    r = i; c = j;
-                }
             }
         }
-        answer += v;
-        System.out.println(answer);
     }
 
     public static boolean OOB (int r, int c){
